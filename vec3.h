abcb28cb2617eb7a2692ef6580215c9c082ae801
slimkaki <rafaalmada98@gmail.com>
2021-11-11 10:47:18 -0300

    Iniciando o projeto 2
    Co-authoreed-by: Lucas Leal <lucaslealvale01@gmail.com>

---
+++ vec3.h
@@ -0,0 +1,165 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+#include <random>
+
+using std::sqrt;
+
+// Gerar numero aleatorio de 0 a 1
+double random_double2() {
+   // Returns a random real in [0,1).
+   return rand() / (RAND_MAX + 1.0);
+}
+
+double random_double2(double min, double max) {
+   // Returns a random real in [min,max).
+   return min + (max-min)*random_double2();
+}
+
+
+class vec3 {
+    public:
+        vec3() : e{0,0,0} {}
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
+
+        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
+
+        vec3& operator+=(const vec3 &v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        vec3& operator*=(const double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        vec3& operator/=(const double t) {
+            return *this *= 1/t;
+        }
+
+        double length() const {
+            return sqrt(length_squared());
+        }
+
+        double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+
+       inline static vec3 random() {
+           return vec3(random_double2(), random_double2(), random_double2());
+       }
+
+       inline static vec3 random(double min, double max) {
+           return vec3(random_double2(min,max), random_double2(min,max), random_double2(min,max));
+       }
+
+       bool near_zero() const {
+           // Return true if the vector is close to zero in all dimensions.
+           const auto s = 1e-8;
+           return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+       }
+      
+
+   public:
+       double e[3];
+
+
+};
+
+
+// Type aliases for vec3
+using point3 = vec3;   // 3D point
+using color = vec3;    // RGB color
+
+
+
+// vec3 Utility Functions
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+    return (1/t) * v;
+}
+
+//produto escalar
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
+}
+
+//produto vetorial
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+   while (true) {
+       auto p = vec3::random(-1,1);
+       if (p.length_squared() >= 1) continue;
+       return p;
+   }
+}
+
+inline vec3 random_unit_vector() {
+   return unit_vector(random_in_unit_sphere());
+}
+
+// lança um ponto aleatoriamente e ve se o produto escalar é maior que 0
+vec3 random_in_hemisphere(const vec3& normal) {
+   vec3 in_unit_sphere = random_in_unit_sphere();
+   if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+       return in_unit_sphere;
+   else
+       return -in_unit_sphere;
+}
+
+vec3 reflect(const vec3& v, const vec3& n) {
+   return v - 2*dot(v,n)*n;
+}
+
+
+
+#endif
