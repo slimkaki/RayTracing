89a5b9cbd43ab498c7545415c0ee214916e4e377
slimkaki <rafaalmada98@gmail.com>
2021-11-16 18:51:52 -0300

    Atualizando contas do paraboloide
    Co-authored-by: Lucas Leal <lucaslealvale01@gmail.com>

--- paraboloid.h
+++ paraboloid.h
@@ -43,31 +43,34 @@ bool paraboloid::hit(const ray &r, double t_min, double t_max, hit_record &rec) 
     double directionY = r.direction()[1];
     double directionZ = r.direction()[2];
 
-    // if (directionY - originY < 10) return false;
+    // if (directionZ - originZ > 5.0) return false;
     
     auto a = (value_A*value_A*directionY*directionY) + (value_B*value_B*directionX*directionX);
     //std::cerr << "a: " << a << std::endl;
-    auto half_b = value_A*value_A*directionY*originY + value_B*value_B*directionX*originX - directionZ*value_A*value_A*value_B*value_B/2;
+    auto b = (-2*value_A*value_A*directionY*originY) + (-2*value_B*value_B*directionX*originX) + (-directionZ*value_A*value_A*value_B*value_B);
     //std::cerr << "half_b: " << half_b << std::endl;
-    auto c = originY*originY + originX*originX + originZ*value_A*value_A*value_B*value_B;
+    auto c = (originY*originY) + (originX*originX) + (originZ*value_A*value_A*value_B*value_B);
     //std::cerr << "c: " << c << std::endl;
 
-    auto discriminant =  half_b*half_b - a * c;
+    auto discriminant =  b*b - 4 * a * c;
     if (discriminant < 0)
         return false;
     auto sqrtd = sqrt(discriminant);
 
     // Find the nearest root that lies in the acceptable range.
-    auto root = (-half_b - sqrtd) / a;
+    auto root = (-b - sqrtd) / (2 * a);
     if (root < t_min || t_max < root) {
-        root = (-half_b + sqrtd) / a;
+        root = (-b + sqrtd) / (2 * a);
         if (root < t_min || t_max < root)
             return false;
     }
 
+    // std::cerr << "root: " << root << "\n";
+    // if (root > 5.0) return false;
+
     rec.t = root;
     rec.p = r.at(rec.t);
-    vec3 outward_normal = (rec.p - center) / value_A*value_B;
+    vec3 outward_normal = (rec.p - center) / value_A*value_A*value_B*value_B;
     rec.set_face_normal(r, outward_normal);
     rec.mat_ptr = mat_ptr;
     // std::cerr << "true\n"  <<std::endl;
