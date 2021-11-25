28f20546720021e5a63e3a274bfc9254fd327e69
slimkaki <rafaalmada98@gmail.com>
2021-11-25 11:49:03 -0300

    Comecando a entrega

--- material.h
+++ material.h
@@ -2,6 +2,7 @@
 #define MATERIAL_H
 
 #include "rtweekend.h"
+#include "texture.h"
 
 struct hit_record;
 
@@ -13,25 +14,26 @@ class material {
 };
 
 class lambertian : public material {
-   public:
-       lambertian(const color& a) : albedo(a) {}
+    public:
+        lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+        lambertian(shared_ptr<texture> a) : albedo(a) {}
 
-       virtual bool scatter(
-           const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
-       ) const override {
-           auto scatter_direction = rec.normal + random_unit_vector();
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            auto scatter_direction = rec.normal + random_unit_vector();
 
-           // Catch degenerate scatter direction
-           if (scatter_direction.near_zero())
-               scatter_direction = rec.normal;
+            // Catch degenerate scatter direction
+            if (scatter_direction.near_zero())
+                scatter_direction = rec.normal;
 
-           scattered = ray(rec.p, scatter_direction);
-           attenuation = albedo;
-           return true;
-       }
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo->value(rec.u, rec.v, rec.p);
+            return true;
+        }
 
-   public:
-       color albedo;
+    public:
+        shared_ptr<texture> albedo;
 };
 
 
@@ -89,6 +91,21 @@ class dielectric : public material {
             return r0 + (1-r0)*pow((1 - cosine),5);
         }
 };
+
+
+// Implementando classe de marmore
+// class marble : public material {
+//     public:
+//          marble(const color& a) : albedo(make_shared<solid_color>(a)) {}
+//          marble(shared_ptr<texture> a) : albedo(a) {}
+
+//         virtual color_value(double u,double v, const point3& p) const override {
+//             return color(1,1,1) * noise.noise(p);
+//         }
+
+//     public:
+//         perlin noise;
+// };
 
 
 #endif
